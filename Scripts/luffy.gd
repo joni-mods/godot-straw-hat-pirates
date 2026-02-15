336e597fb11ebd35e81baa183b9e948f53d4598f
Joel Albarran <joel.albarran@outlook.de>
2026-02-15 16:19:19 +0100

    modified movement logic for luffy

--- Scripts/luffy.gd
+++ Scripts/luffy.gd
@@ -1,34 +1,36 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_FORCE = 300.0
 
-@onready var sprite = $AnimatedSprite2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var animation = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -JUMP_FORCE
-	
-	var direction = Input.get_axis("run_left", "run_right")
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("run_left", "run_right")
 	if direction != 0:
-		sprite.flip_h = (direction == -1)
-	
-	velocity.x = direction * SPEED
-	move_and_slide()
-	
-	update_animation(direction)
-	
-func update_animation(direction) -> void:
-	if is_on_floor():
-		if direction == 0:
-			sprite.play("idle")
-		else:	
-			sprite.play("run")
+		velocity.x = direction * SPEED
 	else:
-		if velocity.y < 0:
-			sprite.play("jump")
-		else:
-			sprite.play("fall")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction and is_on_floor():	
+		animation.flip_h = (direction != 1)
+		animation.play("run")
+	elif velocity.y > 0:
+		animation.play("fall")
+	elif velocity.y < 0:
+		animation.play("jump")		
+	else:
+		animation.play("idle")
+
+	move_and_slide()
