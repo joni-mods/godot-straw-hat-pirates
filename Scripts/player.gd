64cb03856343138f8d234800166586fab18cca5f
Joel Albarran <joel.albarran@outlook.de>
2026-02-21 21:29:31 +0100

    added sword stab attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,37 +1,67 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -200.0
 
 @onready var animation = $AnimatedSprite2D
 
 var hasSword = false
+var isAttacking = false
+var toggleAttack = false
+
+func checkAttack():
+	if Input.is_action_just_pressed("ui_stab"):
+		isAttacking = true
+		$StabArea/CollisionShape2D.disabled = false
+		animation.play("stab")
+		if animation.flip_h:
+			$StabArea/CollisionShape2D.position.x *= -1
+			toggleAttack = true
 
 func _physics_process(delta: float) -> void:
 	var swordExtra = ""
 	if hasSword:
 		swordExtra = "_sword"
 
+	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if isAttacking == false:
+		# jump  
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("run_left", "run_right")
-	if direction != 0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# run 
+		var direction := Input.get_axis("run_left", "run_right")
+		if direction != 0:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if direction and is_on_floor():	
-		animation.flip_h = (direction != 1)
-		animation.play("run" + swordExtra)
-	elif velocity.y > 0:
-		animation.play("fall" + swordExtra)
-	elif velocity.y < 0:
-		animation.play("jump" + swordExtra)		
-	else:
-		animation.play("idle" + swordExtra)
+		# change facing
+		if direction:
+			animation.flip_h = (direction != 1)
+		
+		# play animations	
+		if direction and is_on_floor():	
+			animation.play("run" + swordExtra)
+		elif velocity.y > 0:
+			animation.play("fall" + swordExtra)
+		elif velocity.y < 0:
+			animation.play("jump" + swordExtra)		
+		else:
+			animation.play("idle" + swordExtra)
 
+	if hasSword:
+		checkAttack()
+	
 	move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	isAttacking = false
+	$StabArea/CollisionShape2D.disabled = true
+	if toggleAttack:
+		$StabArea/CollisionShape2D.position.x *= -1
+		toggleAttack = false
+	
