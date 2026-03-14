cd65096361cb76c317392e1bd4eb072f2a5d3b51
Joel Albarran <joel.albarran@outlook.de>
2026-03-14 21:24:59 +0100

    added destructive barrels

--- Scripts/barrel.gd
+++ Scripts/barrel.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@export var hp = 5
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("FromPlayer"):
-		print("truuump")	
+		hp -= 1
+		print(hp)
+		if hp > 0:
+			$AnimatedSprite2D.play("hit")
+		else:
+			$AnimatedSprite2D.play("destroyed")
+			$GPUParticles2D.emitting = true
+			await $AnimatedSprite2D.animation_finished
+			queue_free()
+		
