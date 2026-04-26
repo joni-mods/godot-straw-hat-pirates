181ea40e4e46f6c4e228fea1b4912526e764e62c
Joel Albarran <joel.albarran@outlook.de>
2026-04-26 16:48:13 +0200

    added cannons and fire effects for them

---
+++ Scripts/shooter.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var minWaitTime = 2
+@export var maxWaitTime = 6
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	while(true):
+		randomize()
+		var waitTimer = randi_range(minWaitTime, maxWaitTime)
+		await get_tree().create_timer(waitTimer).timeout
+		fire()
+		
+func fire():
+	$Cannon.play("fire")
+	await get_tree().create_timer(0.2).timeout
+	$FireEffect.visible = true
+	$FireEffect.play("fire")
+	await get_tree().create_timer(0.3).timeout
+	$FireEffect.visible = false
