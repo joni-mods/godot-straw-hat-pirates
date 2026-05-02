012232b805f071f71b0ff887ae7d09ba5ac2abf7
Joel Albarran <joel.albarran@outlook.de>
2026-05-02 23:58:10 +0200

    added direction to the cannons

--- Scripts/shooter.gd
+++ Scripts/shooter.gd
@@ -2,15 +2,21 @@ extends Node2D
 
 @export var minWaitTime = 2
 @export var maxWaitTime = 6
+@export var lookLeft = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if(lookLeft == false):
+		$Cannon.flip_h = true
+		$FireEffect.position.x *= -1
+		$FireEffect .flip_h = true
+		
 	while(true):
-		randomize()
+		randomize() 
 		var waitTimer = randi_range(minWaitTime, maxWaitTime)
 		await get_tree().create_timer(waitTimer).timeout
 		fire()
-		
+
 func fire():
 	$Cannon.play("fire")
 	await get_tree().create_timer(0.2).timeout
@@ -18,3 +24,4 @@ func fire():
 	$FireEffect.play("fire")
 	await get_tree().create_timer(0.3).timeout
 	$FireEffect.visible = false
+	
