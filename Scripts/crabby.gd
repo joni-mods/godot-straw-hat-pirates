7cf6903c6642ec8d2372b2842eaffc3fc2f39cef
Joel Albarran <joel.albarran@outlook.de>
2026-09-05 14:30:13 +0200

    added crabby's death

--- Scripts/crabby.gd
+++ Scripts/crabby.gd
@@ -5,34 +5,52 @@ const JUMP_VELOCITY = -400.0
 
 @export var hp : =3
 var direction := 1
+var isDead := false
+
+func _ready():
+	checkStatus()
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if is_on_floor():
+	if !isDead && is_on_floor():
 		if is_on_wall():
 			direction *= -1
 		
 		if direction == 1:
 			$AnimatedSprite2D.flip_h = true
-			velocity.x = direction * SPEED
 		elif direction == -1:
 			$AnimatedSprite2D.flip_h = false
-			velocity.x = direction * SPEED
-			
-		if velocity.x != 0:
-			$AnimatedSprite2D.play("run")
+
+		velocity.x = direction * SPEED
 
 	move_and_slide()
 
+func run():
+	$AnimatedSprite2D.play("run")
+
+func checkStatus():
+	if hp <= 0:
+		isDead = true
+		direction = 0
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("death")
+		await get_tree().create_timer(1).timeout
+		queue_free()
+	else:
+		run()
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	checkStatus()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("AttackArea"):
+	if !isDead && area.is_in_group("AttackArea"):
 		hp -= 1
 		direction = 0
 		$AnimatedSprite2D.play("hit")
-		print("args array")
-		await $AnimatedSprite2D.animation_finished
+		print("urgh")
+		await get_tree().create_timer(1).timeout
 		direction = 1
+		checkStatus()
